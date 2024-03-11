06479dacd2490ce5bcb190ef3c733083f5eb9020
Rishith Gandham <rishith.gandham@gmail.com>
2024-03-10 22:01:07 -0400

    energy options added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "green-energy": "#27FF00"
+      },
+      screens: {
+        '-2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        '-xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        '-lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        '-md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        '-sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      }
     },
   },
   plugins: [],
